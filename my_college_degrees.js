977c1db9db06796ce3d14cfb670137981a1320de
dvelasquez124 <89713276+dvelasquez124@users.noreply.github.com>
2023-12-04 22:04:26 -0500

    Add files via upload

---
+++ my_college_degrees.js
@@ -0,0 +1,36 @@
+const api = "https://splendid-duckanoo-33e6bf.netlify.app/my_college_degrees.json";
+
+function getDegree() {
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            const div = document.getElementById("college_degree_list");
+            div.innerHTML = buildDegreeList(data);
+        })
+        .catch(error => console.log(error))
+}
+
+function buildDegreeList(degrees) {
+    const projectList = degrees.map(degree => {
+        return `<div class="item">
+            <dt>
+                <strong>${degree.school}</strong>
+            </dt>
+            <dd>
+                ${degree.program_major}
+            </dd>
+            <dd
+                ${degree.type}
+            </dd>
+            <dd
+                ${degree.year_conferred}
+            </dd>
+        </div>
+        `
+    })
+    return `
+    <dl class="projects">
+    ${projectList.join('')}
+    </dl>
+    `;
+}
